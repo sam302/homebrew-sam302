f4d13f646762fd31fd94036cba2ef5dffda3cafd
sam302 <sam302psu@gmail.com>
2025-09-16 10:42:15 -0400

    Update goprox.rb

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -1,8 +1,8 @@
 class Goprox < Formula
   desc "GoPro media management tool"
-  homepage "https://github.com/fxstein/GoProX"
+  homepage "https://github.com/sam302/GoProX"
   version "v01.14.00"
-  url "https://github.com/fxstein/GoProX/archive/vv01.14.00.tar.gz"
+  url "https://github.com/sam302/GoProX/releases/download/v01.14.00/goprox-v01.14.00.tar.gz"
   sha256 "af6d8b1b870b3024ae7c157e837a1ef103ab6528af8c9b2f498c89a10c65fd31"
   
   depends_on "zsh"
